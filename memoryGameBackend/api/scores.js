ee53d686afbf98387a5cf613b17ee06e0c4c502a
SubVray <66286368+SubVray@users.noreply.github.com>
2023-07-31 21:35:38 -0600

    update

--- memoryGameBackend/api/scores.js
+++
@@ -1,7 +0,0 @@
-const { faces } = require("./themes");
-
-function scoresHandler(req, res) {
-  res.json(faces);
-}
-
-module.exports = scoresHandler;
--- memoryGameBackend/index.js
+++ memoryGameBackend/index.js
@@ -1,8 +1,12 @@
-const express = require("express");
+const cors = require("cors");
 const app = express();
-
+const { food, animals, cars, faces } = require("./themes");
+app.use(cors());
 app.get("/api", (req, res) => {
   res.send("hola");
+});
+app.get("api/scores", (req, res) => {
+  res.send(faces);
 });
 
 module.exports = app;
