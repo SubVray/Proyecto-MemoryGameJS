c1b2615d7dc65b86428aa47f525d3bab27a35ddf
SubVray <66286368+SubVray@users.noreply.github.com>
2023-08-02 03:14:09 -0600

    mongoose

--- memoryGameBackend/index.js
+++ memoryGameBackend/index.js
@@ -1,8 +1,21 @@
 const { food, animals, vehicles, faces } = require("./themes");
+const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(cors());
+
+// La URL de conexión se leerá de la variable de entorno en Vercel
+const dbUri = process.env.MONGODB_URI;
+
+// Conecta a la base de datos
+mongoose
+  .connect(dbUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Conexión exitosa a MongoDB Atlas"))
+  .catch((err) => console.error("Error al conectar a MongoDB Atlas:", err));
 
 app.get("/", (req, res) => {
   res.send("Api Funcionado");
